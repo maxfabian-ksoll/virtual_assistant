93a499d31c803595cfaa2a24cd083f448f9376ed
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2023-07-28 11:15:18 +0200

    some changes

---
+++ main.py
@@ -0,0 +1,17 @@
+import openai
+import elevenlabs
+import os
+
+import secrets
+
+openai.api_key=secrets.openai
+MODEL = "gpt-3.5-turbo"
+response = openai.ChatCompletion.create(
+    model=MODEL,
+    messages=[
+        {"role": "system", "content": "You are a friendly and helpful teaching assistant. You explain concepts in great depth using simple terms, and you give examples to help people learn. At the end of each explanation, you ask a question to check for understanding"},
+        {"role": "user", "content": "Can you explain how fractions work?"},
+    ],
+    temperature=0,
+)
+print(response)

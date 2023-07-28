8614e9ecb4bcdb8189e9fa90a3c3fd4455dfa985
Max-Fabian Ksoll <maxfabian.ksoll@eudemonia-solutions.de>
2023-07-28 12:02:04 +0200

    ask as function

--- main.py
+++ main.py
@@ -6,25 +6,28 @@ import secrets
 import messages
 import model
 
-newMessage="Cool es läuft"
-messages.history.append({"role": "user", "content": "Cool es läuft"})
 
-openai.api_key = secrets.openai
-MODEL = "gpt-3.5-turbo"
-response = openai.ChatCompletion.create(
-    model=MODEL,
-    messages=model.model + messages.history + [{"role": "user", "content": "Cool es läuft"}],
-    temperature=0,
-)
-responseContent = response["choices"][0]["message"]["content"]
-messages.history.append({"role": "system", "content": responseContent})
+def ask_chat_gpt(newMessage: str):
+    # newMessage="Cool es läuft"
+    messages.history.append({"role": "user", "content": "Cool es läuft"})
 
-with open("messages.py", "w") as f:
-    f.write("history=[\n")
-    for line in messages.history:
-        f.write("    %s,\n" % line)
-    f.write("]\n")
-    f.close()
+    openai.api_key = secrets.openai
+    MODEL = "gpt-3.5-turbo"
+    response = openai.ChatCompletion.create(
+        model=MODEL,
+        messages=model.model + messages.history + [{"role": "user", "content": "Cool es läuft"}],
+        temperature=0,
+    )
+    responseContent = response["choices"][0]["message"]["content"]
+    messages.history.append({"role": "system", "content": responseContent})
 
-print(responseContent)
+    with open("messages.py", "w") as f:
+        f.write("history=[\n")
+        for line in messages.history:
+            f.write("    %s,\n" % line)
+        f.write("]\n")
+        f.close()
+
+    print(responseContent)
+    return responseContent
 
